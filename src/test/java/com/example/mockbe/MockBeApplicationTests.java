ce69c054fa9f089ac5eb28d8d003b9052ef4b980
hai1710 <125845377+hai1710@users.noreply.github.com>
2023-12-25 09:51:56 +0700

    add spring

---
+++ src/test/java/com/example/mockbe/MockBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
